8c8ccdee07885fd1fc617a0abd0ecb122b6eda0e
Phillip Hoff <phillip@orst.edu>
2021-06-01 05:15:31 +0000

    Sketch parsing palette metadata.

--- src/NauticalCharts.Tests/BsbMetadataReaderTests.cs
+++ src/NauticalCharts.Tests/BsbMetadataReaderTests.cs
@@ -5,7 +5,7 @@ namespace NauticalCharts.Tests
     public class BsbMetadataReaderTests
     {
         [Fact]
-        public void ReadSize()
+        public void ReadSizeMetadata()
         {
             var metadata = BsbMetadataReader.ReadMetadata(new[] { new BsbTextEntry("BSB", new[] { "NA=CONTINUATION A,NU=344102,RA=1171,2098,DU=254" }) });
 
@@ -13,6 +13,33 @@ namespace NauticalCharts.Tests
             Assert.NotNull(metadata.Size);
             Assert.Equal(2098, metadata.Size.Height);
             Assert.Equal(1171, metadata.Size.Width);
+        }
+
+        [Fact]
+        public void ReadPrimaryPalette()
+        {
+            var metadata = BsbMetadataReader.ReadMetadata(
+                new[] {
+                    new BsbTextEntry("RGB", new[] { "1,0,0,0" }),
+                    new BsbTextEntry("RGB", new[] { "2,255,255,255" }),
+                    new BsbTextEntry("RGB", new[] { "3,201,208,184" }),
+                });
+
+            Assert.NotNull(metadata);
+            Assert.NotNull(metadata.Palette);
+            Assert.Equal(3, metadata.Palette.Count);
+            Assert.True(metadata.Palette.ContainsKey(1));
+            Assert.Equal(0, metadata.Palette[1].R);
+            Assert.Equal(0, metadata.Palette[1].G);
+            Assert.Equal(0, metadata.Palette[1].B);
+            Assert.True(metadata.Palette.ContainsKey(2));
+            Assert.Equal(255, metadata.Palette[2].R);
+            Assert.Equal(255, metadata.Palette[2].G);
+            Assert.Equal(255, metadata.Palette[2].B);
+            Assert.True(metadata.Palette.ContainsKey(3));
+            Assert.Equal(201, metadata.Palette[3].R);
+            Assert.Equal(208, metadata.Palette[3].G);
+            Assert.Equal(184, metadata.Palette[3].B);
         }
     }
 }
--- src/NauticalCharts/BsbMetadataReader.cs
+++ src/NauticalCharts/BsbMetadataReader.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace NauticalCharts
@@ -20,6 +21,7 @@ namespace NauticalCharts
                 metadata = textEntry.EntryType switch
                 {
                     "BSB" => ParseSize(metadata, textEntry),
+                    "RGB" => ParsePrimaryPalette(metadata, textEntry),
                     _ => metadata
                 };
             }
@@ -41,6 +43,29 @@ namespace NauticalCharts
                     int width = Int32.Parse(match.Groups["width"].Value);
 
                     return metadata with { Size = new BsbSize(height, width) };
+                }
+            }
+
+            return metadata;
+        }
+
+        private static Regex PaletteRegex = new Regex("^(?<index>\\d+),(?<r>\\d+),(?<g>\\d+),(?<b>\\d+)$");
+
+        private static BsbMetadata ParsePrimaryPalette(BsbMetadata metadata, BsbTextEntry textEntry)
+        {
+            foreach (var line in textEntry.Lines)
+            {
+                var match = PaletteRegex.Match(line);
+
+                if (match.Success)
+                {
+                    byte index = Byte.Parse(match.Groups["index"].Value);
+                    byte r = Byte.Parse(match.Groups["r"].Value);
+                    byte g = Byte.Parse(match.Groups["g"].Value);
+                    byte b = Byte.Parse(match.Groups["b"].Value);
+
+                    // TODO: Think of a better way to copy existing palette.
+                    return metadata with { Palette = new Dictionary<byte, BsbColor>(metadata.Palette ?? Enumerable.Empty<KeyValuePair<byte, BsbColor>>()) { { index, new BsbColor(r, g, b) } } };
                 }
             }
 
