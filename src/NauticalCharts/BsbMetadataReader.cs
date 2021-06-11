5a8f8291409baf1781d1f50f5686f98f2cd383de
Phillip Hoff <phillip@orst.edu>
2021-06-10 22:22:30 -0700

    Refactor metadata reading.

--- src/NauticalCharts/BsbMetadataReader.cs
+++ src/NauticalCharts/BsbMetadataReader.cs
@@ -14,16 +14,30 @@ namespace NauticalCharts
                 throw new ArgumentNullException(nameof(textEntries));
             }
 
-            var metadata = new BsbMetadata();
+            var entryMap = new Dictionary<string, IList<BsbTextEntry>>();
 
             foreach (var textEntry in textEntries)
             {
-                metadata = textEntry.EntryType switch
+                IList<BsbTextEntry> entries;
+
+                if (!entryMap.TryGetValue(textEntry.EntryType, out entries))
                 {
-                    "BSB" => ParseSize(metadata, textEntry),
-                    "RGB" => ParsePrimaryPalette(metadata, textEntry),
-                    _ => metadata
-                };
+                    entryMap[textEntry.EntryType] = entries = new List<BsbTextEntry>();
+                }
+
+                entries.Add(textEntry);
+            }
+
+            var metadata = new BsbMetadata();
+
+            if (entryMap.TryGetValue("BSB", out IList<BsbTextEntry> bsbEntries))
+            {
+                metadata = ParseSize(metadata, bsbEntries);
+            }
+
+            if (entryMap.TryGetValue("RGB", out IList<BsbTextEntry> rgbEntries))
+            {
+                metadata = ParsePrimaryPalette(metadata, rgbEntries);
             }
 
             return metadata;
@@ -31,19 +45,20 @@ namespace NauticalCharts
 
         private static Regex SizeRegex = new Regex("RA=(?<width>\\d+),(?<height>\\d+)");
 
-        private static BsbMetadata ParseSize(BsbMetadata metadata, BsbTextEntry textEntry)
+        private static BsbMetadata ParseSize(BsbMetadata metadata, IList<BsbTextEntry> textEntries)
         {
-            foreach (var line in textEntry.Lines)
+            var match =
+                textEntries
+                    .SelectMany(textEntry => textEntry.Lines)
+                    .Select(line => SizeRegex.Match(line))
+                    .FirstOrDefault(match => match.Success);
+
+            if (match != null)
             {
-                var match = SizeRegex.Match(line);
+                int height = Int32.Parse(match.Groups["height"].Value);
+                int width = Int32.Parse(match.Groups["width"].Value);
 
-                if (match.Success)
-                {
-                    int height = Int32.Parse(match.Groups["height"].Value);
-                    int width = Int32.Parse(match.Groups["width"].Value);
-
-                    return metadata with { Size = new BsbSize(height, width) };
-                }
+                return metadata with { Size = new BsbSize(height, width) };
             }
 
             return metadata;
@@ -51,22 +66,30 @@ namespace NauticalCharts
 
         private static Regex PaletteRegex = new Regex("^(?<index>\\d+),(?<r>\\d+),(?<g>\\d+),(?<b>\\d+)$");
 
-        private static BsbMetadata ParsePrimaryPalette(BsbMetadata metadata, BsbTextEntry textEntry)
+        private static BsbMetadata ParsePrimaryPalette(BsbMetadata metadata, IList<BsbTextEntry> textEntries)
         {
-            foreach (var line in textEntry.Lines)
+            var palette =
+                textEntries
+                    .SelectMany(textEntry => textEntry.Lines)
+                    .Select(line => PaletteRegex.Match(line))
+                    .Where(match => match.Success)
+                    .ToDictionary(
+                        match =>
+                        {
+                            return Byte.Parse(match.Groups["index"].Value);
+                        },
+                        match =>
+                        {
+                            byte r = Byte.Parse(match.Groups["r"].Value);
+                            byte g = Byte.Parse(match.Groups["g"].Value);
+                            byte b = Byte.Parse(match.Groups["b"].Value);
+
+                            return new BsbColor(r, g, b);
+                        });
+
+            if (palette.Count > 0)
             {
-                var match = PaletteRegex.Match(line);
-
-                if (match.Success)
-                {
-                    byte index = Byte.Parse(match.Groups["index"].Value);
-                    byte r = Byte.Parse(match.Groups["r"].Value);
-                    byte g = Byte.Parse(match.Groups["g"].Value);
-                    byte b = Byte.Parse(match.Groups["b"].Value);
-
-                    // TODO: Think of a better way to copy existing palette.
-                    return metadata with { Palette = new Dictionary<byte, BsbColor>(metadata.Palette ?? Enumerable.Empty<KeyValuePair<byte, BsbColor>>()) { { index, new BsbColor(r, g, b) } } };
-                }
+                return metadata with { Palette = palette };
             }
 
             return metadata;
